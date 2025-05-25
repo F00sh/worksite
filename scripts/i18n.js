eb9cd45890ac1f9a38fb7fb8a9f5dc3956bf25d3
ASoldo <soldo.andrej@gmail.com>
2025-05-25 22:14:08 +0200

    Update

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -1,3 +1,11 @@
-document.addEventListener('partials:loaded', () =>
-  setLang(localStorage.getItem('lang') || 'en')
-);
+document.addEventListener('partials:loaded', () => {
+  import('/scripts/includes.js').then(({ setLang }) => {
+    setLang(localStorage.getItem('lang') || 'en');
+  });
+});
+
+document.addEventListener('lang:change', () => {
+  import('/scripts/includes.js').then(({ setLang }) => {
+    setLang(localStorage.getItem('lang') || 'en');
+  });
+});
